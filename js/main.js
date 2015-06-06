1353da06b2dcbde92f1affffe9b8d77631543543
septimus <septimus98@gmail.com>
2015-06-06 06:07:50 +0000

    Got multiplayer logic mostly in, added textures

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/*global Phaser*/
+/*global Phaser Peer*/
 
 // reference - http://mrmine.com/
 
@@ -13,6 +13,8 @@ var friend = {
     sprite: null,
     decodedState: null
 };
+
+var ground = null;
 
 var stateBinarySchema = [
     'left',
@@ -30,14 +32,14 @@ var Binary = {
         var str = "";
         
         for(var i = 0; i < data.length; i++) {
-            str += (data) ? "1" : "0";
+            str += (data[i]) ? "1" : "0";
         }
         
         return str;
     },
     // returns an object that maps 'data' to 'schema'
     decode: function(schema, data) {
-        if(scheme.length != data.length)
+        if(schema.length != data.length)
             throw new Error("'Binary.decode' scheme.length must equal data.length!");
         
         var obj = {};
@@ -64,7 +66,6 @@ function sendMessage() {
 function onConnection() {
     console.log('Connected!');
     
-    document.getElementById("connect").style.display = "none";
     document.getElementById("send").style.display = "block";
     
     conn.on('data', function(data) {
@@ -75,16 +76,18 @@ function onConnection() {
         
         // only the server should ever get this message
         if(server && data.state) {
+            console.log(data.state);
             friend.decodedState = Binary.decode(stateBinarySchema, data.state);
         }
         
         // only the client should ever get position info
         if(!server) {
             
-            if(data.me.x && data.me.y) {
-                
+            if(me.sprite && data.me && data.me.x && data.me.y) {
+                me.sprite.position.x = data.me.x;
+                me.sprite.position.y = data.me.y;
             }
-            else if(data.friend.x && data.friend.y) {
+            if(friend.sprite && data.friend && data.friend.x && data.friend.y) {
                 friend.sprite.position.x = data.friend.x;
                 friend.sprite.position.y = data.friend.y;
             }
@@ -92,7 +95,7 @@ function onConnection() {
         }
     });
     
-    game = new Phaser.Game(800, 600, Phaser.AUTO, 'MiningGame', playState);
+    game = new Phaser.Game(480, 320, Phaser.AUTO, 'MiningGame', playState);
 }
 
 function connect() {
@@ -102,6 +105,8 @@ function connect() {
 
     if(yourName != '') {
         peer = new Peer(yourName, {key: '55sj0os1x512a9k9'});
+        
+        document.getElementById("connect").style.display = "none";
         
         // we want to be the client
         if(friendsName != '') {
@@ -132,14 +137,20 @@ var playState = {
     },
     
     create: function() {
+        game.stage.disableVisibilityChange = true;
         
-        me.sprite = game.add.sprite(400, 300, 'miningTextures');
+        me.sprite = game.add.sprite(240, 160, 'miningTextures');
         me.sprite.animations.add('idle', [0], 15, false);
         me.sprite.animations.play('idle', 15, false);
         
-        friend.sprite = game.add.sprite(432, 300, 'miningTextures');
+        friend.sprite = game.add.sprite(272, 160, 'miningTextures');
         friend.sprite.animations.add('idle', [0], 15, false);
         friend.sprite.animations.play('idle', 15, false);
+        
+        ground = game.add.sprite(0, 192, 'miningTextures');
+        ground.width = 800;
+        ground.animations.add('ground', [1], 15, false);
+        ground.animations.play('ground', 15, false);
         
         // only the server simulates physics
         if(server) {
@@ -149,18 +160,25 @@ var playState = {
             game.physics.arcade.gravity.y = 250;
             
             // setup physics for me
-            me.sprite.anchor.setTo(0.5, 0.5);
+            me.sprite.anchor.setTo(0, 0);
             me.sprite.smoothed = false;
             game.physics.arcade.enable(me.sprite);
             me.sprite.body.collideWorldBounds = true;
-            me.sprite.body.setSize(32, 32, 0,0);
+            me.sprite.body.setSize(32, 32, 0, 0);
+            me.sprite.body.gravity = new Phaser.Point(0, 9.6);
             
             // setup physics for friend
-            friend.sprite.anchor.setTo(0.5, 0.5);
+            friend.sprite.anchor.setTo(0, 0);
             friend.sprite.smoothed = false;
             game.physics.arcade.enable(friend.sprite);
             friend.sprite.body.collideWorldBounds = true;
-            friend.sprite.body.setSize(32, 32, 0,0);
+            friend.sprite.body.setSize(32, 32, 0, 0);
+            friend.sprite.body.gravity = new Phaser.Point(0, 9.6);
+            
+            game.physics.arcade.enable(ground);
+            ground.body.setSize(800, 200, 0, 0);
+            ground.body.immovable = true;
+            ground.body.allowGravity = false;
         }
     },
     
@@ -174,19 +192,56 @@ var playState = {
         
         if(server) {
             
+            game.physics.arcade.collide(me.sprite, ground);
+            game.physics.arcade.collide(friend.sprite, ground);
+            game.physics.arcade.collide(me.sprite, friend.sprite);
             
+            if(left) {
+                me.sprite.body.velocity.x -= 10;
+            }
+            if(right) {
+                me.sprite.body.velocity.x += 10;
+            }
+            if(up) {
+                me.sprite.body.velocity.y -= 200;
+            }
+            
+            if(friend.decodedState) {
+                if(friend.decodedState.left) {
+                    friend.sprite.body.velocity.x -= 10;
+                }
+                if(friend.decodedState.right) {
+                    friend.sprite.body.velocity.x += 10;
+                }
+                if(friend.decodedState.up) {
+                    friend.sprite.body.velocity.y -= 200;
+                }
+            }
+            
+            conn.send({
+                // me is friend
+                me: {
+                    x: friend.sprite.position.x,
+                    y: friend.sprite.position.y
+                },
+                // friend is me
+                friend: {
+                    x: me.sprite.position.x,
+                    y: me.sprite.position.y
+                }
+            });
             
         }
         else {
             
-            var state = Binary.encode(stateBinarySchema, [left, right, up, down]);
+            var state = Binary.encode([left, right, up, down]);
             
             // only send the state when its been updated
             if(me.encodedState != state) {
                 me.encodedState = state;
                 conn.send({
                     state: state
-                });   
+                });
             }
             
         }
